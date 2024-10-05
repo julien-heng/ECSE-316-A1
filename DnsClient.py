1c3c4c225fe7a9381ed7c75de7d4916d3c12ca18
julien-heng <139326103+julien-heng@users.noreply.github.com>
2024-10-05 12:35:25 -0400

    Update DnsClient.py

--- DnsClient.py
+++ DnsClient.py
@@ -14,27 +14,27 @@ if __name__ == "__main__":
     }
 
     for i in range(1, len(sys.argv)):
-        match sys.argv[i]:
-            case "-t": 
-                if sys.argv[i + 1].isdigit():
+
+        if sys.argv[i] == "-t":
+            if sys.argv[i + 1].isdigit():
                     parameters["timeout"] = sys.argv[i + 1]
                     i = i + 1
-            case "-r":
-                if sys.argv[i + 1].isdigit():
+        elif sys.argv[i] == "-r":
+            if sys.argv[i + 1].isdigit():
                     parameters["max_retries"] = sys.argv[i + 1]
                     i = i + 1
-            case "-p":
-                if sys.argv[i + 1].isdigit():
+        elif sys.argv[i] == "-p":  
+            if sys.argv[i + 1].isdigit():
                     parameters["port"] = sys.argv[i + 1]
-                    i = i + 1
-            case "-mx":
-                parameters["mail_name"] = "mail"
-            case "-nx":
-                parameters["mail_name"] = "name"
-            case _:
-                if sys.argv[i][0] == "@":
-                    parameters["server"] = sys.argv[i]
-                    parameters["name"] = sys.argv[i + 1]
+                    i = i + 1  
+        elif sys.argv[i] == "-mx":
+            parameters["mail_name"] = "mail"
+        elif sys.argv[i] == "-nx":   
+            parameters["mail_name"] = "name"  
+        elif sys.argv[i][0] == "@":
+            parameters["server"] = sys.argv[i]
+            parameters["name"] = sys.argv[i + 1]            
+                
 
     for x in parameters:
         print(f"{x}:{parameters[x]}")
