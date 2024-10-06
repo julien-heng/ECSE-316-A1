6b328ec9205f4ee3ef92dd07bb884595a3fc8fad
julien-heng <139326103+julien-heng@users.noreply.github.com>
2024-10-05 20:37:16 -0400

    Update DnsClient.py

--- DnsClient.py
+++ DnsClient.py
@@ -1,43 +1,51 @@
 import sys
 import socket
 
-
-if __name__ == "__main__":
-
+def set_arguments(args):
     parameters = {
-        "timeout": "5",
-        "max_retries": "3",
-        "port": "53",
-        "mail_name": "",
+        "timeout": 5,
+        "max_retries": 3,
+        "port": 53,
+        "type": "A",
         "server": "",
         "name": ""
     }
 
-    for i in range(1, len(sys.argv)):
-
-        if sys.argv[i] == "-t":
-            if sys.argv[i + 1].isdigit():
-                    parameters["timeout"] = sys.argv[i + 1]
+    for i in range(1, len(args)):
+        if args[i] == "-t":
+            if args[i + 1].isdigit():
+                    parameters["timeout"] = int(args[i + 1])
                     i = i + 1
-        elif sys.argv[i] == "-r":
-            if sys.argv[i + 1].isdigit():
-                    parameters["max_retries"] = sys.argv[i + 1]
+        elif args[i] == "-r":
+            if args[i + 1].isdigit():
+                    parameters["max_retries"] = int(args[i + 1])
                     i = i + 1
-        elif sys.argv[i] == "-p":  
-            if sys.argv[i + 1].isdigit():
-                    parameters["port"] = sys.argv[i + 1]
+        elif args[i] == "-p":  
+            if args[i + 1].isdigit():
+                    parameters["port"] = int(args[i + 1])
                     i = i + 1  
-        elif sys.argv[i] == "-mx":
-            parameters["mail_name"] = "mail"
-        elif sys.argv[i] == "-nx":   
-            parameters["mail_name"] = "name"  
-        elif sys.argv[i][0] == "@":
-            parameters["server"] = sys.argv[i]
-            parameters["name"] = sys.argv[i + 1]            
-                
+        elif args[i] == "-mx":
+            parameters["type"] = "MX"
+        elif args[i] == "-nx":   
+            parameters["type"] = "NX"  
+        elif args[i][0] == "@":
+            parameters["server"] = args[i][1:]
+            parameters["name"] = args[i + 1]  
 
-    for x in parameters:
-        print(f"{x}:{parameters[x]}")
+    return parameters
+
+
+if __name__ == "__main__":
+
+    para = set_arguments(sys.argv)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((para["server"], para["port"]))
+    print("connected")
+    sock.close()
+    print("closed")
+
+    for x in para:
+        print(f"{x}:{para[x]}")
                 
 
 
